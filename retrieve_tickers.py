9b219a737e08121490c5370dfb7261072c488481
Chayapon Lee-Isranukul <chayaponlee-isranukul@Chayapons-MacBook-Air.local>
2022-02-08 17:06:14 +0700

    Update .gitignore file and retrieve_tickers.py

--- retrieve_tickers.py
+++ retrieve_tickers.py
@@ -5,7 +5,7 @@ import nasdaqdatalink as ndl
 import time
 import logging as log
 
-
+log.basicConfig(level='INFO')
 
 """
 Retrieve American stock data
@@ -16,7 +16,9 @@ For MacAir: you can configure the file at
 """
 
 # Get ticket information: list of tickers
+log.info("Fetching ticker metadata")
 tickers_metadata = ndl.get_table('SHARADAR/TICKERS', paginate=True)
+log.info("Done")
 
 # Filtering exchanges and categories of stocks
 filter_exchange = ['NYSE', 'NASDAQ', 'NYSEMKT']
@@ -36,14 +38,16 @@ focused_stocks_ticker = tickers_metadata[
 stacked_data = []
 
 s_time_chunk = time.time()
-for i, ticker in enumerate(focused_stocks_ticker[0:]):
+log.info("Fetching historical data")
+for i, ticker in enumerate(focused_stocks_ticker):
     data = ndl.get_table('SHARADAR/SEP', ticker=ticker,
                          paginate=True, date={'gte': '2012-01-01'}).sort_values(['ticker', 'date'])
     stacked_data.append(data)
-    if i % 100 == 0:
+    if i % 100 == 0 and i != 0:
         print(f'Tickers iterated: {i}')
 e_time_chunk = time.time()
 
+log.info("Done")
 log.info("Total download time: ", (e_time_chunk-s_time_chunk), "sec")
 
 # Concat into one dataframe
