102aeb29cb25c5aa736231ced1bf4eaf80208f80
Chayapon Lee-Isranukul <cleei@sertiscorp.com>
2022-02-09 01:08:15 +0700

    Add exception handlers when fetching historical data, incase of network failure

--- retrieve_tickers.py
+++ retrieve_tickers.py
@@ -2,6 +2,7 @@ import pandas as pd
 import nasdaqdatalink as ndl
 import time
 import logging as log
+from requests.exceptions import ChunkedEncodingError
 
 log.basicConfig(level='INFO')
 
@@ -32,25 +33,40 @@ filter_cat = ['Domestic Common Stock', 'ADR Common Stock',
 focused_stocks_ticker = tickers_metadata[
     (tickers_metadata.exchange.isin(filter_exchange)) & (tickers_metadata.category.isin(filter_cat))].ticker.unique()
 
+log.info(f"Total tickers: {len(list(focused_stocks_ticker))}")
+
 # Retrieve data via for loop (might be a more efficient way to do this)
 stacked_data = []
 
 s_time_chunk = time.time()
 log.info("Fetching historical data")
 for i, ticker in enumerate(focused_stocks_ticker):
-    data = ndl.get_table('SHARADAR/SEP', ticker=ticker,
-                         paginate=True, date={'gte': '2012-01-01'}).sort_values(['ticker', 'date'])
+    # log.info(i)
+    try_cnt = 0
+    while try_cnt <= 20:
+        try:
+            data = ndl.get_table('SHARADAR/SEP', ticker=ticker, paginate=True, date={'gte': '2012-01-01'})
+            break
+        except ChunkedEncodingError as ex:
+            if try_cnt > 20:
+                log.info(f'Error (after trying multiple times): {ex}')
+                log.info(f"Failed to fetch {ticker}")
+                break
+            else:
+                try_cnt += 1
+                time.sleep(5)
+                log.info("Failed: Retrying")
+
     stacked_data.append(data)
     if i % 100 == 0 and i != 0:
-        print(f'Tickers iterated: {i}')
+        log.info(f'Tickers iterated: {i}')
 e_time_chunk = time.time()
 
 log.info("Done")
-log.info("Total download time: ", (e_time_chunk-s_time_chunk), "sec")
+log.info("Total download time: ", (e_time_chunk - s_time_chunk), "sec")
 
 # Concat into one dataframe
 stacked_hist = pd.concat(stacked_data)
+stacked_hist.sort_values(['ticker', 'date'], inplace=True)
 
 stacked_hist.to_csv('Data/stacked_hist.csv', index=False)
-
-
