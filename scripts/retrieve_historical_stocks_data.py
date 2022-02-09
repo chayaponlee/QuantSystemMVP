673f1d655f54dfdeb311fd913d5f5e6b50251e42
Chayapon Lee-Isranukul <cleei@sertiscorp.com>
2022-02-10 02:30:29 +0700

    Make changes to retrieve_historical_stocks_data.py

--- scripts/retrieve_historical_stocks_data.py
+++ scripts/retrieve_historical_stocks_data.py
@@ -1,7 +1,7 @@
 import sys
 import os
 import logging
-from quantlib.data_utils import retrieve_historical_stocks
+from quantlib.data_utils import retrieve_historical_stocks, extend_dataframe
 from quantlib import general_utils as gu
 from quantlib import qlogger
 
@@ -10,9 +10,10 @@ project_path = os.getenv('QuantSystemMVP')
 
 
 if __name__ == '__main__':
-    stocks_data = retrieve_historical_stocks()
+    stocks_df, stocks_wide_df, available_tickers = retrieve_historical_stocks()
+    stocks_extended_df = extend_dataframe( available_tickers, stocks_wide_df)
 
     save_path = f'{project_path}/Data/stock_hist.obj'
-    gu.save_file(save_path, stocks_data)
+    gu.save_file(save_path, (stocks_df, stocks_wide_df, stocks_extended_df, available_tickers))
 
     logger.info(f"Historical Stock data written to {save_path}")
