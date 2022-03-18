237d66203c4d4892bd709a0a67680fe88cf2924c
Chayapon Lee-Isranukul <cleei@sertiscorp.com>
2022-03-18 17:11:21 +0700

    Add interface class to Engineering classes

--- realgam/quantlib/engineer/interface.py
+++ realgam/quantlib/engineer/interface.py
@@ -1,10 +1,39 @@
 from abc import ABC, abstractmethod
-import json
 import pandas as pd
+from typing import Union, List
+
+pd.options.mode.chained_assignment = None
 
 
-class Engineer(ABC):
+class BaseEngineer(ABC):
 
-    def __init__(self, financial_df):
-        self.f_df = financial_df
+    def __init__(self, financial_df: pd.DataFrame):
+        self.__df = financial_df.copy()
 
+    @abstractmethod
+    def df(self):
+        return self.__df
+
+    @abstractmethod
+    def set_df(self, financial_df: pd.DataFrame):
+        self.__df = None
+        self.__df = financial_df.copy()
+
+
+class GroupBaseEngineer(ABC):
+
+    def __init__(self, financial_df: pd.DataFrame, groupby_col: Union[str, List]):
+        self.__df = financial_df.copy()
+        self.__groupby_col = groupby_col
+
+    @abstractmethod
+    def df(self):
+        return self.__df
+
+    @abstractmethod
+    def set_df(self, financial_df: pd.DataFrame):
+        self.__df = financial_df.copy()
+
+    @abstractmethod
+    def groupby_col(self):
+        return self.__groupby_col
